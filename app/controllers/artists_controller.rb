293c925d1d51a6b5c3d7855a5984a017ebf36adf
Emily Truong <truong.emily@hotmail.com>
2020-10-03 12:06:23 +1000

    did create edit and destory actions for artists

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,40 @@
 class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:show, :edit, :destroy]
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    @artist.user = current_user
+    if @artist.save
+      redirect_to artist_path(@artist)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @artist.update(artist_params)
+    redirect_to artist_path(@artist)
+  end
+
+  def destroy
+    @artist.destroy
+    redirect_to artists_path
+  end
+
+  private
+
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
+
+  def artist_params
+    params.require(:artist).permit(:description, :qualifications, photos: [])
+  end
 end
