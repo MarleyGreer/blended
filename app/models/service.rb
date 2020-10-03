3982c7370775b1f96cddb713de911697fdf0c12d
Marley Greer <marley.greer93@gmail.com>
2020-10-03 13:23:39 +1000

    added all crud actions and views for services.

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,2 +1,7 @@
 class Service < ApplicationRecord
+  belongs_to :artist
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :time, presence: true
 end
