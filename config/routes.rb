239f56625346f5e4382b763863166fbbdd41ce6c
Emily Truong <truong.emily@hotmail.com>
2020-10-03 15:36:53 +1000

    changed time to duration in services and made it an integer

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,22 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :artists do
     resources :services, only: [:new, :create, :index]
+    resources :bookings, only: [:new, :create]
   end
 
   resources :services, only: [:destroy, :edit, :update]
+  resources :bookings, only: [:index, :show, :edit, :update, :destroy] do
+    collection do
+      get :pending
+      get :confirmed
+      get :declined
+      get :cancelled
+    end
+    member do
+      patch :confirm
+      patch :decline
+      patch :cancel
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_01_102813) do
+ActiveRecord::Schema.define(version: 2020_10_03_053520) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2020_10_01_102813) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
@@ -80,10 +81,10 @@ ActiveRecord::Schema.define(version: 2020_10_01_102813) do
     t.string "name"
     t.text "description"
     t.integer "price"
-    t.datetime "time"
     t.bigint "artist_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "duration"
     t.index ["artist_id"], name: "index_services_on_artist_id"
   end
 
