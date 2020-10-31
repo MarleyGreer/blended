6be41ba3bdd1f89052159e8abaf838a44e71423b
Trent Skurrie <trent.skurrie@jacmax.com.au>
2020-10-31 11:12:19 +1100

    holidays updated

--- app/javascript/components/holidays.js
+++ app/javascript/components/holidays.js
@@ -4,17 +4,28 @@ $(document).on("click", '#addHoliday', function() {
     var holiday_date = input1.split("-");
     var new_date = new Date(holiday_date[2], holiday_date[1]-1 , holiday_date[0]);
     var holidayarray = [];
-    var olddates = document.getElementById('holidayhidden').value.split(",");
-    console.log(olddates);
-    console.log(new_date);
-    olddates.forEach(element => {
-        holidayarray.push(element)
-    });
-    holidayarray.push(new_date);
-    console.log(input1);
-    console.log(holiday_date);
-    console.log(holidayarray);
-    document.getElementById('holidaydate').value = "";
-    document.getElementById('holidaydate').dataset.holiday = holidayarray;
-    document.getElementById('holidayhidden').value = holidayarray;
+    if (document.getElementById('holidayhidden') != null) {
+        var olddates = document.getElementById('holidayhidden').value.split(",");
+
+        olddates.forEach(element => {
+            holidayarray.push(element)
+        });
+    }
+        holidayarray.push(new_date);
+        holidayarray.sort();
+        document.getElementById('holidaydate').value = "";
+        document.getElementById('holidaydate').dataset.holiday = holidayarray;
+        document.getElementById('holidayhidden').value = holidayarray;
+
+        var holidaydiv = document.getElementById("holidayslist");
+        holidaydiv.innerHTML = "";
+        
+        holidayarray.forEach(holidayfunction);
+        
+        function holidayfunction(value) {
+            if (value != "") {
+            holidaydiv.innerHTML += `<p>- ${value} </p>`;
+            }
+        }  
+
 });
